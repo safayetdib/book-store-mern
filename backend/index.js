b381178e6e37ffca5e0ab28d05750552d196344e
safayet <safayetdib@gmail.com>
2023-09-05 23:28:22 +0600

    added crud operation and cors

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,30 @@
 import express, { request, response } from 'express';
 import mongoose from 'mongoose';
 import { PORT, mongoDBURL } from './config.js';
-import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
 
 const app = express();
 
 // middleware for parsing request body
 app.use(express.json());
 
+// middleware for cors
+// option 1: allow all origins with default of cors(*)
+// app.use(cors());
+// option 2: allow specific origins
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST', 'PUT', 'DELETE'],
+		allowedHeaders: ['Content-Type'],
+	})
+);
+
 app.get('/', (request, response) => {
 	return response.status(234).send('Welcome');
 });
 
-// route for save a new book
-app.post('/books', async (request, response) => {
-	try {
-		if (
-			!request.body.title ||
-			!request.body.author ||
-			!request.body.publishYear
-		) {
-			return response.status(400).send({
-				message: 'Send all required fields: title, author, publishYear',
-			});
-		}
-
-		const newBook = {
-			title: request.body.title,
-			author: request.body.author,
-			publishYear: request.body.publishYear,
-		};
-
-		const book = await Book.create(newBook);
-		return response.status(201).send(book);
-	} catch (error) {
-		console.log(error);
-		response.status(500).send({ message: error.message });
-	}
-});
-
-// route for get all books from database
-app.get('/books', (request, response) => {});
+app.use('/books', booksRoute);
 
 mongoose
 	.connect(mongoDBURL)
