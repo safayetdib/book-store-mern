5fd4190d60bc073446551f5b0874cdd720765fcc
safayet <safayetdib@gmail.com>
2023-09-05 21:42:26 +0600

    added book post api route

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,46 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import mongoose from 'mongoose';
 import { PORT, mongoDBURL } from './config.js';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+// middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (request, response) => {
 	return response.status(234).send('Welcome');
 });
+
+// route for save a new book
+app.post('/books', async (request, response) => {
+	try {
+		if (
+			!request.body.title ||
+			!request.body.author ||
+			!request.body.publishYear
+		) {
+			return response.status(400).send({
+				message: 'Send all required fields: title, author, publishYear',
+			});
+		}
+
+		const newBook = {
+			title: request.body.title,
+			author: request.body.author,
+			publishYear: request.body.publishYear,
+		};
+
+		const book = await Book.create(newBook);
+		return response.status(201).send(book);
+	} catch (error) {
+		console.log(error);
+		response.status(500).send({ message: error.message });
+	}
+});
+
+// route for get all books from database
+app.get('/books', (request, response) => {});
 
 mongoose
 	.connect(mongoDBURL)
